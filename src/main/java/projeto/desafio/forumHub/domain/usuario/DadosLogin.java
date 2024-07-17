db3ba4e71de4a1edb17d576863f4b9f422b834e2
Yuri Bonifacio <157377299+YuriBoni@users.noreply.github.com>
2024-07-16 21:29:17 -0300

    Add files via upload

---
+++ src/main/java/projeto/desafio/forumHub/domain/usuario/DadosLogin.java
@@ -0,0 +1,15 @@
+package projeto.desafio.forumHub.domain.usuario;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+
+public record DadosLogin(
+        @NotBlank
+        @Email
+        String email,
+
+        @NotBlank
+        String password
+) {
+}
